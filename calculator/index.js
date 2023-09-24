70730e43a50788677246cba098bde93a85ae37f1
Mallikarjun-salaki <142146684+Mallikarjun-salaki@users.noreply.github.com>
2023-09-25 00:00:29 +0530

    Add files via upload

---
+++ calculator/index.js
@@ -0,0 +1,29 @@
+function turnOff(){
+    var inputfield = document.getElementById("screen")
+    inputfield.style.visibility ="hidden"  
+}
+function turnOn(){
+    var inputfield = document.getElementById("screen")
+    inputfield.style.visibility ="visible"  
+}
+
+
+var inputValues = document.getElementById("inputnumbers")
+
+function getValues(num){
+    inputValues.value += num
+    console.log(inputValues.value)
+}
+
+function clearOneChar(){
+    inputValues.value = inputValues.value.slice(0,-1)
+}
+
+function clearAll(){
+    inputValues.value =""
+}
+
+
+function calculate(){
+    inputValues.value = eval(inputValues.value)
+}
